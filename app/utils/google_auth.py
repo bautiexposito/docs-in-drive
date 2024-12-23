b8e5e99dedbe77780fa9567a36bd824067711651
bautiexposito <bautistaexposito@outlook.com>
2024-12-23 20:34:03 -0300

    autenticacion con google drive

--- app/utils/google_auth.py
+++ app/utils/google_auth.py
@@ -1,10 +1,27 @@
 from pydrive2.auth import GoogleAuth
-import os
+from pydrive2.drive import GoogleDrive
 
-current_dir = os.path.dirname(__file__)
-settings_path = os.path.abspath(os.path.join(current_dir, '../config/settings.yaml'))
-
+# Autenticacion con Google Drive
 gauth = GoogleAuth()
-#gauth.settings['client_config_file'] = 'credentials/client_secret.json'
-gauth.settings_file = settings_path
-gauth.LocalWebserverAuth()
+
+# Ruta donde se guardaran las credenciales
+gauth.settings_file = "settings.yaml"
+gauth.LoadClientConfigFile("C:/Users/Marcelo/Documents/justSomeCodes!/Inventario/credentials/client_secrets.json")
+
+# Intentar cargar credenciales guardadas
+try:
+    gauth.LoadCredentialsFile("C:/Users/Marcelo/Documents/justSomeCodes!/Inventario/credentials/credentials_module.json")
+    if gauth.credentials is None or gauth.access_token_expired:
+        # Si no existen o estan expiradas, reautenticar
+        gauth.LocalWebserverAuth()
+        gauth.SaveCredentialsFile("C:/Users/Marcelo/Documents/justSomeCodes!/Inventario/credentials/credentials_module.json")
+    else:
+        print("Autenticacion cargada desde archivo")
+except Exception as e:
+    # Si no existe el archivo o hay un error, forzar autenticacion
+    print("Autenticacion fallida o no encontrada, solicitando nueva...")
+    gauth.LocalWebserverAuth()
+    gauth.SaveCredentialsFile("C:/Users/Marcelo/Documents/justSomeCodes!/Inventario/credentials/credentials_module.json")
+
+# Inicializar Google Drive
+drive = GoogleDrive(gauth)
